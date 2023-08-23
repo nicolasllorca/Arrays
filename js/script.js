ed33d78dba7c77cdb927caddd544ab3e0bf5bf7a
nicolasllorca <nicolasllorca21@gmail.com>
2023-08-23 18:53:00 -0300

    carpeta agregada

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  
+  const elementosFiltrados = strangeArray.filter(element => typeof element === 'string');
+  const elementosOrdenados = elementosFiltrados.sort ()
+  
+  showList(elementosOrdenados);
+ 
+
+});
